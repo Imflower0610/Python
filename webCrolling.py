865cced7e8a7e6678eb3a25b3d3524b319ed735a
Imflower0610 <110653050+Imflower0610@users.noreply.github.com>
2022-12-22 14:51:15 +0900

    fix : modify the python webcrolling

--- webCrolling.py
+++ webCrolling.py
@@ -75,17 +75,82 @@ print(ul)
 # for i in li:
 #     print(i.text)
 
-url = "https://www.naver.com" #naver url 저장
-html = urlopen(url)#url open
-html = html.read()# naver sorce 읽기
-print(html)
+# url = "https://www.naver.com" #naver url 저장
+# html = urlopen(url)#url open
+# html = html.read()# naver sorce 읽기
+# print(html)
+
+# bsp = bs4.BeautifulSoup(html, "html.parser")
+# temp = bsp.findAll("strong",{"class","current"})
+# print(temp)
+# # 현재기온 몇도인것만 출력하려면?
+# print(temp[0].text)
+# for t in temp:
+#     if "현재기온" in t:
+#         print(t.text)
+
+
+# nav = bsp.findAll("a",{"class","nav"})
+# for n in nav:
+#     print(n.text)
+
+
+# url = "https://news.naver.com/main/list.naver?mode=LPOD&mid=sec&sid1=001&sid2=140&oid=001&isYeonhapFlash=Y&aid=0013655846" 
+# html= urlopen(url)
+# html = html.read()
+
+# bsp = bs4.BeautifulSoup(html, "html.parser")
+# news_ul = bsp.find("ul",{"class","type02"})
+# print(len(news_ul)) # 길이가 1로 찍히면 하나라는 거니까 find로 바꿔줌
+# news_li = news_ul.findAll("li")
+# for li in news_li:
+#     strong = li.find("strong")
+#     print(strong.text)
+
+# 내가푼거
+# url = "https://comic.naver.com/webtoon/weekdayList?week=thu"
+# html= urlopen(url)
+# html = html.read()
+
+# bsp = bs4.BeautifulSoup(html, "html.parser")
+# thu_title = bsp.find("ul",{"class","img_list"})
+# print(len(thu_title)) 
+# thudt = thu_title.findAll("dt")
+# for dt in thudt:
+#     thu_dt = dt.find("a")
+#     print(thu_dt.text)
+
+# 쌤이 푼거
+# url = "https://comic.naver.com/webtoon/weekday"
+# html= urlopen(url)
+# html = html.read()
+
+# bsp = bs4.BeautifulSoup(html, "html.parser")
+# div = bsp.findAll("div",{"class","col_inner"})
+# ul = div[3].find("ul")
+# lis = ul.findAll("li")
+# for li in lis:
+#     title = li.find("a",{"class","title"})
+#     print(title.text)
+
+url = "https://www.op.gg/statistics/champions"
+html= urlopen(url)
+html = html.read()
 
 bsp = bs4.BeautifulSoup(html, "html.parser")
-temp = bsp.findAll("strong",{"class","current"})
-print(temp)
-# 현재기온 몇도인것만 출력하려면?
-print(temp[0].text)
-for t in temp:
-    if "현재기온" in t:
-        print(t.text)
+body = bsp.find("tbody")
+td = body.findAll("td")
+print(td)
+for r in td:
+    td = r.find("div", {"class","css-qzg52u e1alsbyt0"})
+    print(td)
+    # div = td.find("div",{"class","css-qzg52u e1alsbyt0"})
+    # if int(div.text)>50:
+    #     jjang = td.find("strong")
+    #     print(jjang.text)
+# lis = ul.findAll("li")
+# for li in lis:
+#     title = li.find("a",{"class","title"})
+#     print(title.text)
 
+ 
